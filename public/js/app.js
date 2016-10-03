736fb07bcbf31a851f268086857f0c284f2acdb2
Oona <oona.salmipuro@metropolia.fi>
2016-10-04 01:06:26 +0300

    added front-end form to submit messages

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,19 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
 	console.log('New message: ' + message.text);
 });
+
+//handles submitting new messages
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	//to clear the input field after sending the message
+	$message.val('');
+});
